71900447b781f358555187c028a1dbebc76e2ab0
paulovictorlisboa <inoventer.furg@gmail.com>
2023-01-25 15:11:04 -0300

    Initial commit

---
+++ data/ETA_Charqueada_MOV_5.js
@@ -0,0 +1,8 @@
+var json_ETA_Charqueada_MOV_5 = {
+"type": "FeatureCollection",
+"name": "ETA_Charqueada_MOV_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "ETA", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -51.621184351271417, -29.954352532087494 ], [ -51.621290461616077, -29.954015325176844 ], [ -51.621034186987472, -29.953950100714223 ], [ -51.620929067888007, -29.954292555679714 ], [ -51.621184351271417, -29.954352532087494 ] ] ] ] } }
+]
+}
